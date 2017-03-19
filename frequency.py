cca8e5dd918e5b5b915d1692130f210967b5164f
William Derksen <william.derksen@students.olin.edu>
2017-03-19 16:52:12 -0400

    Toolbox Finished

--- frequency.py
+++ frequency.py
@@ -2,7 +2,8 @@
 Project Gutenberg """
 
 import string
-
+from collections import Counter
+import operator
 
 def get_word_list(file_name):
     """ Reads the specified project Gutenberg book.  Header comments,
@@ -10,7 +11,15 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r') #Open file
+    lines = f.readlines() #read lines from file
+    curr_line = 0 # start line iterator
+    while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1: #while not find the line 'START OF THIS PROJECT GUTENBERG EBOOK'
+      curr_line += 1 #add to iterator
+    lines = lines[curr_line+1:] #return lines after the start of ebook
+    joined_lines = "\n".join(lines) #put back together lines into string
+    joined_lines.replace(string.punctuation, "").lower() #replace punctuation and make all lowercase
+    return joined_lines.split() #split text into list
 
 
 def get_top_n_words(word_list, n):
@@ -21,10 +30,12 @@ def get_top_n_words(word_list, n):
     punctuation
     n: the number of words to return
     returns: a list of n most frequently occurring words ordered from most
-    frequently to least frequentlyoccurring
+    frequently to least frequently occurring
     """
-    pass
+    c = Counter(word_list) #get dictionary counting frequency of words
+    word_sort = sorted(c.items(), key=operator.itemgetter(1), reverse=True) #sort dictionary into list of decreasing frequency
+    return word_sort[:n] #return the values up to the nth word.
 
 if __name__ == "__main__":
-    print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    w_list = get_word_list('pg32325.txt') # get word list from Gutenberg text
+    print(get_top_n_words(w_list, 100)) #print the 100 most common words.
